0e7a157f45be37c8cde4805ff8cb2f9f89724dfa
amira-saya <amirasayari9@gmail.com>
2021-02-24 21:24:40 +0100

    c

--- backend/server.js
+++ backend/server.js
@@ -42,3 +42,9 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
