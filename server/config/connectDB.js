19e84a9f7f93df274b91aa8d087fc030d2b52e99
amira-saya <amirasayari9@gmail.com>
2021-02-26 11:40:45 +0100

    a

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const path = require("path")
-// const config = require("config");
-const fs = require('fs')
-let data_static = fs.readFileSync(path.resolve(__dirname),'./default.json')
-let config = JSON.parse(data_static)
+// const path = require("path")
+// // const config = require("config");
+// const fs = require('fs')
+// let data_static = fs.readFileSync(path.resolve(__dirname),'./default.json')
+// let config = JSON.parse(data_static)
 
-const mongoUri = process.env.MONGODB_URI || config[mongoUri]
+const mongoUri = process.env.MONGODB_URI || "mongodb+srv://amira:azerty@cluster0.jljxp.mongodb.net/users_db?retryWrites=true&w=majority"
 
 module.exports = connectDB = async () => {
   try {
