ec70d086f34eb3c25d71ed3bebc258c76c9ccba2
amira-saya <amirasayari9@gmail.com>
2021-02-26 11:37:16 +0100

    path fix

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const path = require("path")
 // const config = require("config");
 const fs = require('fs')
-let data_static = fs.readFileSync('./default.json')
+let data_static = fs.readFileSync(path.resolve(__dirname),'./default.json')
 let config = JSON.parse(data_static)
 
 const mongoUri = process.env.MONGODB_URI || config[mongoUri]
